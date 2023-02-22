ead733a15dc2fb0a5086996676c87974bab97ebb
Shantanu Kumar <97434805+Shantanu29kumar@users.noreply.github.com>
2023-02-23 01:22:48 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,105 @@
+import discord
+import requests
+import json
+import random
+from replit import db
+import os
+from keep_alive import keep_alive
+
+intents = discord.Intents.all()
+client = discord.Client(command_prefix='!', intents=intents)
+
+sad_words = ["sad","heartbroken","unhappy","depressed","miserable","sorry","bad","melancholy","upset","depressing","angry"]
+
+starter_encouragements = [
+  "Cheer Up!",
+  "Hang in there.",
+  "You are a great person",
+  "You are not alone",
+  "We go gym!"
+]
+
+if "responding" not in db.keys():
+  db["responding"] = True
+
+def get_quote():
+  reponse = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(reponse.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return (quote)
+
+def update_encouragements(encouraging_message):
+  if "encouragements" in db.keys():
+    encouragements = db["encouragements"]
+    encouragements.append(encouraging_message)
+    db["encouragements"] = encouragements
+  else:
+    db["encouragements"] = [encouraging_message]
+    
+def delete_encouragement(index):
+  encouragements = db["encouragements"]
+  if len(encouragements) > index:
+    del encouragements[index]
+  db["encouragements"] = encouragements
+  
+  
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  msg = message.content
+  
+  if msg.startswith('hello'):
+    await message.channel.send('Hello!')
+
+  if msg.startswith('inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+  if db["responding"]:
+    options = starter_encouragements
+    if "encouragement" in db.keys():
+      options = options + db["encouragements"]
+  
+    if any(word in msg for word in sad_words):
+      await message.channel.send(random.choice(options))
+
+  if msg.startswith("new"):
+    encouraging_message = msg.split("new ",1)[1]
+    update_encouragements(encouraging_message)
+    await message.channel.send("New encouraging message added.")
+
+  if msg.startswith("del"):
+    encouragements = []
+    if "encouragements" in db.keys():
+      index = int(msg.split("del",1)[1])
+      delete_encouragement(index)
+      encouragements = db["encouragements"]
+    await message.channel.send(encouragements)
+      
+  if msg.startswith("list"):
+    encouragements = []
+    if "encouragements" in db.keys():
+      encouragements = db["encouragements"]
+    await message.channel.send(encouragements)
+
+  if msg.startswith("responding"):
+    value = msg.split("responding ",1)[1]
+
+    if value.lower() == "true":
+      db["responding"] = True
+      await message.channel.send("Responding is ON")
+    else:
+      db["responding"] = False
+      await message.channel.send("Responding is OFF")
+    
+keep_alive()
+
+my_secret = os.getenv("TOKEN")
+client.run(my_secret)
